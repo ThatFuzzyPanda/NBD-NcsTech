f72855143c64d7593a0b83e2db37235a3e748ccc
Josh <Silentbutbviolet011@gmail.com>
2024-01-23 22:44:19 -0500

    Vhien added the bootstrap and css to the homepage and this is a new solution file

---
+++ NBDProject/NBDProject/Startup.cs
@@ -0,0 +1,19 @@
+﻿namespace NBDProject
+{
+    public class Startup
+    {
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // ... (other middleware configuration)
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
